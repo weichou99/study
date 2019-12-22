fa6cf4dc918453320c9627aefe7eca7e63826758
weichou99 <weichou99@gmail.com>
2019-12-22 17:36:17 +0800

    commons exec test

--- ApacheCommonsExec/src/main/java/cc/study/App.java
+++ ApacheCommonsExec/src/main/java/cc/study/App.java
@@ -1,13 +1,26 @@
 package cc.study;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			String line = "java -version";
+			CommandLine cmdLine = CommandLine.parse(line);
+			DefaultExecutor executor = new DefaultExecutor();
+			executor.setExitValue(0);
+			ExecuteWatchdog watchdog = new ExecuteWatchdog(90);
+			executor.setWatchdog(watchdog);
+			int exitValue = executor.execute(cmdLine);
+			System.out.println(exitValue);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
